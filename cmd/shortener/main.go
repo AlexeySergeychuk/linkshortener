bf82e0385ea87f2ccee995c18222d58cbd98d833
Alexey Sergeychuk <>
2024-07-21 18:46:41 +0300

    iter 6
    Добавлен middleware логгер реквеста и респонса

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 
 	"github.com/AlexeySergeychuk/linkshortener/internal/app/config"
 	"github.com/AlexeySergeychuk/linkshortener/internal/app/handlers"
@@ -12,6 +13,17 @@ import (
 func main() {
 	config.ParseFlags()
 
+	// создаём предустановленный регистратор zap
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		// вызываем панику, если ошибка
+		panic(err)
+	}
+	defer logger.Sync()
+
+	// делаем регистратор SugaredLogger
+	sugar := *logger.Sugar()
+
 	mapStorage := make(map[string]string)
 	repo := repository.NewRepo(mapStorage)
 	shortLinkStub := shortener.NewShortLink()
@@ -19,6 +31,7 @@ func main() {
 	handler := handlers.NewHandler(shortener)
 
 	router := gin.Default()
+	router.Use(handlers.WithLogging(&sugar))
 	router.POST("/", handler.CreateLinkHandler)
 	router.GET("/:id", handler.GetLinkHandler)
 
